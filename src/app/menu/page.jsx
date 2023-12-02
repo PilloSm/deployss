cdbd7787327fcf9f6547064fd65768fc921b8b16
PilloSm <corzo.arguello.luis@gmail.com>
2023-12-02 13:47:02 -0600

    fasdf

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,3 +1,38 @@
-export default function MENU(){
-    return <h1>hola munod</h1>
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CardCocina from "@/components/CardCocina";
+export default function YourComponent() {
+  const [menuData, setMenuData] = useState(null);
+
+  useEffect(() => {
+    const fetchMenuData = async () => {
+      try {
+        const res = await axios.get(
+          "http://localhost:3000/api/apiCliente/menu"
+        );
+        setMenuData(res.data);
+      } catch (error) {
+        console.error("Error fetching menu data:", error);
+      }
+    };
+
+    fetchMenuData();
+  }, []); // The empty dependency array ensures that this effect runs once when the component mounts
+
+  return (
+    <form className="bg-white flex flex-col items-center justify-center w-full overflow-hidden min-h-screen">
+      <div className="w-full max-w-screen-2xl h-full flex flex-wrap justify-around bg-white relative">
+        <div className="absolute top-4 md:top-8 lg:top-12 left-1/2 transform -translate-x-1/2 font-nunito font-normal text-black text-4xl md:text-5xl lg:text-6xl text-center leading-normal tracking-normal">
+          Menú
+        </div>
+
+        <div className="mt-16 md:mt-24 lg:mt-32 w-full max-w-screen-2xl h-full flex flex-wrap items-center justify-center">
+          {menuData.map((item) => (
+            <CardCocina key={item.id} comidas={item} />
+          ))}
+        </div>
+      </div>
+    </form>
+  );
 }
--- src/components/CardCocina.jsx
+++ src/components/CardCocina.jsx
@@ -1,54 +1,31 @@
-import axios from "axios";
-
-export default function CardPedidos({
-  id_pedido,
-  nombres_comidas,
-  cantidades_detalles,
-  id_cuenta,
-  estado_siguiente_nombre,
-  estado_actual_nombre,
-  estado_actual_id,
-}) {
-  const handleEditar = async (e) => {
-    const res = await axios.put(
-      `/api/apiCafeteria/cambiarEstadoPedido/${id_pedido}`,
-      { estado: estado_actual_id }
-    );
-  };
-  const handleCancelar = async (e) => {
-    const res = await axios.post(
-      `/api/apiCafeteria/cambiarEstadoPedido/${id_pedido}`,
-      { estado: 1 }
-    );
-
-  };
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+const CardCocina = ({ comidas }) => {
   return (
-    <div className="relative mx-2 w-[303px] h-[402px] top-[30px]">
-      <div className="relative h-full">
-        <div className="w-[303px] h-[350px] top-[52px] bg-[#f4f5f6] rounded-[40px] shadow-md relative left-0"></div>
-        <div className="absolute w-[204px] top-[59px] left-[40px] font-nunito font-normal text-black text-[28px] text-center leading-normal tracking-normal">
-          <p className="relative top-[20px] font-bold">{nombres_comidas}</p>
-          <p className="absolute left-[25px] w-[190px] top-[110px] text-left">Cantidad: {cantidades_detalles}</p>
-          <p className="absolute left-[30px] w-[190px] top-[160px] text-left">Usuario: {id_cuenta}</p>
-          
-        </div>
-        <button
-          onClick={handleEditar}
-          className="absolute w-[120px] h-[100px] bottom-[20px] left-[10px] bg-[#25a18ee6] rounded-[20px] border-none cursor-pointer leading-normal text-center flex items-center justify-center text-white text-[16px]"
-        >
-          <div className="pedir">{estado_siguiente_nombre}</div>
-        </button>
-        {estado_actual_nombre == "pedido" ? (
-          <button
-            onClick={handleCancelar}
-            className="absolute w-[120px] h-[100px] bottom-[20px] left-[160px] bg-[#c22323e6] rounded-[20px] border-none cursor-pointer leading-normal text-center flex items-center justify-center text-white text-[16px]"
-          >
-            <div className="pedir">Cancelar Pedido</div>
+    <Link href={`/client/comida?comida=${comidas.id_comidas}`}>
+      <div className="relative mx-2 w-[303px] h-[402px] top-[30px] cursor-pointer">
+        <div className="relative h-full">
+          <div className="w-[303px] h-[350px] top-[52px] bg-[#f4f5f6] rounded-[40px] shadow-md relative left-0"></div>
+          <img
+            className="w-[227px] h-[227px] left-[38px] absolute top-0 object-cover rounded-full"
+            width={277}
+            height={277}
+              src={comidas.imagen}
+            alt={comidas.nombre}
+          />
+          <div className="absolute w-[204px] top-[249px] left-[40px] font-nunito font-normal text-black text-[28px] text-center leading-normal tracking-normal">
+            {comidas.nombre}
+          </div>
+          <button className="absolute w-[240px] h-[40px] bottom-[20px] left-[30px] bg-[#25a18ee6] rounded-[20px] border-none cursor-pointer leading-normal text-center flex items-center justify-center text-white text-[16px]">
+            <div className="pedir">Ver</div>
           </button>
-        ) : (
-          <h1 className="absolute w-[120px] h-[100px] bottom-[0px] left-[185px] left-1px font-bold text-[40px]">NO</h1>
-        )}
+        </div>
       </div>
-    </div>
+      <br />
+      <br />
+    </Link>
   );
-}
+};
+
+export default CardCocina;
