0d6bb1f20b13d7c2d29c4fc649a457d8ecef3123
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-03 16:53:22 -0600

    si

--- src/app/admin/actualizarIngredientes/page.jsx
+++ src/app/admin/actualizarIngredientes/page.jsx
@@ -40,7 +40,7 @@ export default function Actualizar() {
   return (
     <>
       <div className="flex items-center justify-center h-screen">
-        <div className="w-[600px] shadow-lg rounded-[50px] p-8">
+        <div className="w-[600px] bg-white shadow-lg rounded-[50px] p-8">
           <h1 className="text-center text-2xl font-nunito font-bold text-black mb-2">
             Actualizar Ingredientes
           </h1>
