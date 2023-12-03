8a7d085718baab76d605a09176bc041514943b1b
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-03 15:27:50 -0600

    si

--- src/app/client/pedidos/[id]/page.jsx
+++ src/app/client/pedidos/[id]/page.jsx
@@ -3,7 +3,7 @@ import ValUsu from "@/components/validarUsu";
 
 export default function pagina ({ params }) {
   return (
-    <div>
+    <div className="bg-white min-h-screen">
   <div className="absolute top-[20px] md:top-[40px] lg:top-[60px] left-[50%] transform translate-x-[-50%] font-nunito font-normal text-black text-[40px] md:text-[50px] lg:text-[60px] text-center leading-normal tracking-normal">
     Historial de Pedidos
   </div>
