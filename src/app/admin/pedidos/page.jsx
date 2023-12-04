b75f2277ed326a12bc6e563b0be667280d1b1dd7
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-03 23:39:12 -0600

    gt

--- src/app/admin/pedidos/page.jsx
+++ src/app/admin/pedidos/page.jsx
@@ -23,8 +23,6 @@ function Pedidos() {
     }
   };
 
-  console.log(data1)
-
   useEffect(() => {
     fetchData();
 
@@ -45,6 +43,7 @@ function Pedidos() {
         <div className="mt-16 md:mt-24 lg:mt-32 w-full max-w-screen-2xl h-full flex flex-wrap items-center justify-center">
 
         {data1.map((detalle) => (
+
             <CardPedidos
               key={detalle.id_pedido} // Agrega una clave única para cada elemento en el array
               id_pedido={detalle.id_pedido}
