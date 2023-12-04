cc8b7a5d093a73b617c2f7dae4da11ae3250f2f1
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-03 22:19:02 -0600

    liga italia serie a

--- src/components/tablaExtras.jsx
+++ src/components/tablaExtras.jsx
@@ -19,7 +19,7 @@ export default function TablaExtras() {
   }, []);
 
   return (
-    <div className="bg-gradient-to-r from-gray-300 via-gray-400 to-white p-8">
+    <div className="bg-white">
   <div className="text-center text-2xl font-nunito font-bold text-black mb-2">Tabla de Extras</div>
   <table className="w-full mt-5 border-collapse">
     <thead>
