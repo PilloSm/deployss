d912d50a008265df492b3810f8bdb1869a136c03
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-03 19:42:18 -0600

    asereje

--- src/app/admin/ingredientes/page.jsx
+++ src/app/admin/ingredientes/page.jsx
@@ -87,7 +87,7 @@ export default function Ingredientes() {
             type="text"
             name="nombre"
             placeholder="Nombre"
-            className="w-[438px] h-[35px] text-black font-nunito font-normal text-black bg-[white] text-[24px] leading-normal tracking-normal border border-[#797979] outline-none px-3"
+            className="w-[438px] h-[35px] text-black font-nunito font-normal text-black bg-[white] text-[18px] leading-normal tracking-normal border border-[#797979] outline-none px-3"
             onChange={handleCahnge}
           />
         </div>
@@ -98,7 +98,7 @@ export default function Ingredientes() {
           </label>
           <select
             name="unidad"
-            className="w-[438px] h-[35px] font-nunito font-normal bg-[white] text-black text-[24px] leading-normal tracking-normal border border-[#797979] outline-none px-3"
+            className="w-[438px] h-[35px] font-nunito font-normal bg-[white] text-black text-[18px] leading-normal tracking-normal border border-[#797979] outline-none px-3"
             onChange={handleCahnge}
           >
             <option value="">Seleccionar Unidad</option>
@@ -118,7 +118,7 @@ export default function Ingredientes() {
             type="number"
             name="cantidad"
             placeholder="Cantidad"
-            className="w-[438px] h-[35px] text-black font-nunito font-normal text-black bg-[white] text-[24px] leading-normal tracking-normal border border-[#797979] outline-none px-3"
+            className="w-[438px] h-[35px] text-black font-nunito font-normal text-black bg-[white] text-[18px] leading-normal tracking-normal border border-[#797979] outline-none px-3"
             onChange={handleCahnge}
           />
         </div>
@@ -131,7 +131,7 @@ export default function Ingredientes() {
             type="number"
             name="precio"
             placeholder="Precio"
-            className="w-[438px] h-[35px] text-black font-nunito font-normal text-black bg-[white] text-[24px] leading-normal tracking-normal border border-[#797979] outline-none px-3"
+            className="w-[438px] h-[35px] text-black font-nunito font-normal text-black bg-[white] text-[18px] leading-normal tracking-normal border border-[#797979] outline-none px-3"
             onChange={handleCahnge}
           />
         </div>
