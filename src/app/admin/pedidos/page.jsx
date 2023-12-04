e48c2e36f1c2428c2ffcb2d30346c17e143e18d2
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-03 23:54:36 -0600

    aa

--- src/app/admin/pedidos/page.jsx
+++ src/app/admin/pedidos/page.jsx
@@ -1,65 +1,9 @@
-"use client";
-import CardPedidos from "@/components/CardPedidos";
-import axios from "axios";
-import { useState, useEffect } from "react";
-import Link from "next/link";
-function Pedidos() {
-  const [data1, setData1] = useState([]);
-  const [data2, setData2] = useState([]);
-  const [data3, setData3] = useState([]);
-  const [data4, setData4] = useState([]);
-  const fetchData = async () => {
-    try {
-      const res1 = await axios.get(`/api/apiCafeteria/pedidos/2`);
-      const res2 = await axios.get(`/api/apiCafeteria/pedidos/3`);
-      const res3 = await axios.get(`/api/apiCafeteria/pedidos/4`);
-      const res4 = await axios.get(`/api/apiCafeteria/pedidos/5`);
+import React from 'react'
 
-      setData1(res1.data);
-      setData2(res2.data);
-      setData3(res3.data);
-      setData4(res4.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-    console.log(data2)
-    const interval = setInterval(() => {
-      fetchData();
-    }, 60 * 1000);
-
-    return () => clearInterval(interval);
-  }, []);
+const page = () => {
   return (
-    <div className="bg-white h-screen flex flex-row justify-center w-full">
-      <div className="w-full max-w-[1440px] h-full flex flex-wrap justify-around bg-white relative">
-        <div className="h-[120px] w-[120px] flex flex-col items-center justify-center font-nunito font-bold text-black text-center leading-normal tracking-normal">
-          <div className="absolute text-[36px]">Pedidos</div>
-        </div>
-
-        <div className="mt-16 md:mt-24 lg:mt-32 w-full max-w-screen-2xl h-full flex flex-wrap items-center justify-center">
-          {data1.map((detalle) => (
-            <CardPedidos
-              key={detalle.id_pedido} // Agrega una clave única para cada elemento en el array
-              id_pedido={detalle.id_pedido}
-              nombres_comidas={detalle.nombres_comidas}
-              cantidades_detalles={detalle.cantidades_detalles}
-              id_cuenta={detalle.id_cuenta}
-              estado_siguiente_nombre={detalle.estado_siguiente_nombre}
-              estado_actual_nombre={detalle.estado_actual_nombre}
-              estado_actual_id={detalle.estado_actual_id}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    <div>as</div>
+  )
 }
 
-export default Pedidos;
-
-
-
+export default page
