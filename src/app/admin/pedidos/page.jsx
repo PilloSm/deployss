76d45c3e3c4934f22662677a1892b93b903a2e5e
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-03 23:46:10 -0600

    aaaa

--- src/app/admin/pedidos/page.jsx
+++ src/app/admin/pedidos/page.jsx
@@ -2,6 +2,7 @@
 import CardPedidos from "@/components/CardPedidos";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 function Pedidos() {
   const [data1, setData1] = useState([]);
   const [data2, setData2] = useState([]);
@@ -25,7 +26,7 @@ function Pedidos() {
 
   useEffect(() => {
     fetchData();
-
+    console.log(data2)
     const interval = setInterval(() => {
       fetchData();
     }, 60 * 1000);
@@ -33,16 +34,10 @@ function Pedidos() {
     return () => clearInterval(interval);
   }, []);
   return (
-    <div className="bg-white flex flex-row justify-center w-full">
+    <div className="bg-white h-screen flex flex-row justify-center w-full">
       <div className="w-full max-w-[1440px] h-full flex flex-wrap justify-around bg-white relative">
         <div className="h-[120px] w-[120px] flex flex-col items-center justify-center font-nunito font-bold text-black text-center leading-normal tracking-normal">
-          <div className="absolute text-[36px]">Pedidos</div>
-        </div>
-
-
-      <div className="w-full max-w-screen-2xl h-full flex flex-wrap justify-around bg-white relative">
-        <div className="absolute top-4 md:top-8 lg:top-12 left-1/2 transform -translate-x-1/2 font-nunito font-normal text-black text-4xl md:text-5xl lg:text-6xl text-center leading-normal tracking-normal">
-          Menú
+          <div className="absolute text-[36px]">Pedidos 2</div>
         </div>
 
         <div className="mt-16 md:mt-24 lg:mt-32 w-full max-w-screen-2xl h-full flex flex-wrap items-center justify-center">
@@ -60,70 +55,11 @@ function Pedidos() {
           ))}
         </div>
       </div>
-
-
-
-
-        <div className="absolute w-full md:w-[431px] top-[202px] left-[50%] md:left-[572px] font-nunito font-normal text-black text-[60px] text-center leading-normal tracking-normal"></div>
-
-        <div className="relative w-full md:w-[6000px] h-[480px] flex flex-nowrap overflow-scroll justify-start overflow-x-scroll overflow-y-hidden">
-          {data1.map((detalle) => (
-            <CardPedidos
-              key={detalle.id_pedido} // Agrega una clave única para cada elemento en el array
-              id_pedido={detalle.id_pedido}
-              nombres_comidas={detalle.nombres_comidas}
-              cantidades_detalles={detalle.cantidades_detalles}
-              id_cuenta={detalle.id_cuenta}
-              estado_siguiente_nombre={detalle.estado_siguiente_nombre}
-              estado_actual_nombre={detalle.estado_actual_nombre}
-              estado_actual_id={detalle.estado_actual_id}
-            />
-          ))}
-        </div>
-        <div className="absolute w-[431px] top-[820px] left-[570px] font-nunito font-normal text-black text-[64px] text-center leading-normal tracking-normal"></div>
-
-        <div className="flex relative w-[6000px] h-[480px] flex flex-nowrap overflow-scroll justify-start overflow-x-scroll overflow-y-hidden">
-          {data2.map((detalle) => {
-            <CardPedidos
-              key={detalle.id_pedido}
-              nombres_comidas={detalle.nombres_comidas}
-              cantidades_detalles={detalle.cantidades_detalles}
-              id_cuenta={detalle.id_cuenta}
-              estado_siguiente_nombre={detalle.estado_siguiente_nombre}
-            />;
-          })}
-        </div>
-
-        <div className="absolute w-full md:w-[431px] top-[1420px] left-[50%] md:left-[570px] font-nunito font-normal text-black text-[64px] text-center leading-normal tracking-normal"></div>
-
-        <div className="relative w-[6000px] h-[480px] flex flex-nowrap overflow-scroll justify-start overflow-x-scroll overflow-y-hidden">
-          {data3.map((detalle) => {
-            <CardPedidos
-              key={detalle.id_pedido}
-              nombres_comidas={detalle.nombres_comidas}
-              cantidades_detalles={detalle.cantidades_detalles}
-              id_cuenta={detalle.id_cuenta}
-              estado_siguiente_nombre={detalle.estado_siguiente_nombre}
-            />;
-          })}
-        </div>
-
-        <div className="absolute w-full md:w-[431px] top-[2120px] left-[50%] md:left-[570px] font-nunito font-normal text-black text-[64px] text-center leading-normal tracking-normal"></div>
-
-        <div className="relative w-[6000px] h-[480px] flex flex-nowrap overflow-scroll justify-start overflow-x-scroll overflow-y-hidden">
-          {data4.map((detalle) => {
-            <CardPedidos
-              key={detalle.id_pedido}
-              nombres_comidas={detalle.nombres_comidas}
-              cantidades_detalles={detalle.cantidades_detalles}
-              id_cuenta={detalle.id_cuenta}
-              estado_siguiente_nombre={detalle.estado_siguiente_nombre}
-            />;
-          })}
-        </div>
-      </div>
     </div>
   );
 }
 
 export default Pedidos;
+
+
+
