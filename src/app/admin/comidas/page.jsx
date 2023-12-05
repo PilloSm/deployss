2355dc8556ee2877682316cbfe2d86e265526aaf
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-04 18:15:34 -0600

    asddsa

--- src/app/admin/comidas/page.jsx
+++ src/app/admin/comidas/page.jsx
@@ -5,7 +5,7 @@ import AlimentoCrear from "@/components/AlimentoCrear";
 
 export default function IngresarComida() {
   return (
-    <div className="bg-gradient-to-r from-gray-300 via-gray-400 to-white p-8 flex flex-row h-screen justify-center w-full">
+    <div className="min-h-screen bg-gradient-to-r from-gray-300 via-gray-400 to-white p-8 flex flex-row h-screen justify-center w-full">
       <div className="w-[1440px] h-full relative">
         <AlimentoCrear />
       </div>
