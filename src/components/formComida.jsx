e5eaa1ae6eae090c80eab39ecce9e2ed5b65a310
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-05 14:41:43 -0600

    asd

--- src/components/formComida.jsx
+++ src/components/formComida.jsx
@@ -116,11 +116,11 @@ export default function FormComida({ comidas }) {
         {platillo.descripcion}
       </p>
 
-      <p className="absolute w-[325px] top-[447px] left-[119px] font-nunito text-black">
+      <p className="absolute w-[325px] top-[487px] left-[119px] font-nunito text-black">
       <img src="https://i.ibb.co/hZGB6c6/icons8-tiempo-24.png" alt="Tiempo:" />
       </p>
       
-      <p className="absolute w-[325px] top-[447px] left-[149px] font-nunito text-black">
+      <p className="absolute w-[325px] top-[487px] left-[149px] font-nunito text-black text-[30px]">
         {platillo.tiempo_estimado}
       </p>
 
