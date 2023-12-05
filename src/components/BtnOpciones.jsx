50bb8c3ec00922e83e79803124a2d2713124256b
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-05 14:55:13 -0600

    yu

--- src/components/BtnOpciones.jsx
+++ src/components/BtnOpciones.jsx
@@ -67,6 +67,14 @@ const BtnOpciones = () => {
                   />
                   <div>Pedidos</div>
                 </Link>
+                <Link href={"/client/pedidos/" + session.user.id_cuenta} className="text-gray-300 hover:bg-gray-800 hover:border-l-4 hover:border-gray-300 hover:text-white block p-2 transition duration-200">
+                  <img
+                    className="pl-[4px]"
+                    src="/img/sidebar/icons-8-correo-24"
+                    alt=""
+                  />
+                  <div>Notificaciones</div>
+                </Link>
                 </nav>
           </div>
         </div>
