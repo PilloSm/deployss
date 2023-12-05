8b72d701054c65c91e07a489d3200e68da598a7a
nietomar <nietomartinezpedronestor@gmail.com>
2023-12-05 10:58:22 -0600

    a

--- src/app/client/comida/page.jsx
+++ src/app/client/comida/page.jsx
@@ -5,7 +5,7 @@ export default async function Comida(request) {
   const comidaId = searchParams.comida;
 
   return (
-    <div className="bg-white min-h-screen flex flex-row justify-center w-full">
+    <div className="bg-gradient-to-r from-gray-300 via-gray-400 to-white min-h-screen flex flex-row justify-center w-full">
       <div className="bg-white top-[50px] w-[1440px] h-[864px] relative shadow-xl rounded-[50px]">
         <div className="absolute top-[29px] h-[500px]">
           <FormComida comidas={comidaId} />
